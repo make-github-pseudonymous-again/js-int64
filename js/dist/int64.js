b6ac80b2713031caa0a95e6f517b02284c71e9d2
aureooms <aurelien.ooms@gmail.com>
2014-12-22 10:39:04 +0100

    v0.0.2

--- js/dist/int64.js
+++ js/dist/int64.js
@@ -3,6 +3,25 @@
 	'use strict';
 
 
-/* js/src/dummy.js */
+/* js/src/compare.js */
+
+var compare = function ( a , b ) {
+
+	return ( a[0] ^ b[0] ) & 0x80000000 ? ( a[0] > b[0] ? -1 : 1 ) :
+	       a[0] < b[0] ? -1 : a[0] > b[0] ? 1 :
+	       a[1] < b[1] ? -1 : a[1] > b[1] ? 1 : 0 ;
+
+} ;
+
+exports.compare = compare ;
+
+
+/* js/src/limits.js */
+
+/** -2^63 */
+exports.min = [ 0x80000000 , 0x00000000 ] ;
+
+/** 2^63 - 1 */
+exports.max = [ 0x7fffffff , 0xffffffff ] ;
 
 })(typeof exports === 'undefined' ? this['int64'] = {} : exports);
